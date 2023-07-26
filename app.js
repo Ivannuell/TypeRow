dfd5e60fe0c111206c4c886f60b9fed4b65bfa9c
Ivannuell <ivannguminang0@gmail.com>
2023-07-26 13:16:17 +0800

    my mistake

--- app.js
+++ app.js
@@ -158,11 +158,11 @@ function check() {
     }
   })
 
+  typedChar_arr.push(charCheckWrong)
   // TODO fix the counting of correct and wrong char to be used in the calculation of acc and wpm
   //
   // console.log(char_typedCorrect, " = correct ")
   // console.log(char_typedWrong, " = wrong")
-  // typedChar_arr.push(charCheckWrong)
   // console.log(typedChar_arr)
   // console.log("------------------------------ \n")
 
